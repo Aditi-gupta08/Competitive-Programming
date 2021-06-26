2cf3f37a1ec63920b65844c0ce29b98ffea379f6
Aditi Gupta <aditig006@gmail.com>
2021-06-26 17:56:24 +0530

    Few chnages in heapq file

--- heapq.py
+++ heapq.py
@@ -7,6 +7,7 @@ heapq.heappush(h, (3, 'create tests'))
 print(heapq.heappop(h))
 
 
+
 h2 = [(5, 'write code'), (7, 'release product'), (1, 'write spec'), (3, 'create tests')]
 heapq.heapify(h2)
 print(heapq.heappop(h2))
